c1eb1cedb7c6bbee8b4ff953c4e497ee60b029d0
John Bartkiw <14988042+johnbartkiw@users.noreply.github.com>
2019-03-08 14:30:06 -0800

    - First drop of working proof of concept.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # mycroft-skill-iheartradio
-Add internet music streaming from IHeartRadio
+Search and stream internet radio using IHeartRadio
+
+## Description
+Make internet radio streaming work with non-hard coded stations getting streams from IHeartRadio.
+
+## Examples
+* "Play KEXP (on|with|using) I Heart Radio"
+* "Play Jack FM radio"
+* "Play Jazz 24 internet radio"
+* "Play 80s Metal radio (on|with|using) I Heart Radio"
+* "Play 70s Country internet radio (on|with|using) I Heart Radio"
+
+## Examples (Old Intent. May be deprecated after full testing of common play framework)
+* "Stream KEXP"
+* "Stream Jack FM Radio"
+* "Stream Jazz 24 internet radio"
+* "Stream 80s Metal (on|with|using) I Heart Radio"
+* "Stream 70s Country radio (on|with|using) I Heart Radio"
+* "Stream 99.9 internet radio (on|with|using) I Heart Radio"
+* "Stream 107.7 radio station"
+* "Stream NPR News internet radio station"
+* "Stream KEXP radio station (on|with|using) I Heart Radio"
+* "Stream KEXP internet radio station (on|with|using) I Heart Radio"
+
+## Credits
+John Bartkiw
+
+## Category
+**Entertainment**
+
+## Tags
+#stream
+#streaming
+#radio
+#IHeartRadio
--- __init__.py
+++ __init__.py
@@ -7,59 +7,66 @@
 # in the requirements.txt file so the library is installed properly
 # when the skill gets installed later by a user.
 
+import vlc
+import requests
+import json
+
+from .vlc import Instance
 from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.skills.core import MycroftSkill, intent_file_handler
 from mycroft.util.log import LOG
 
-# Each skill is contained within its own class, which inherits base methods
-# from the MycroftSkill class.  You extend this class as shown below.
+# Static values for tunein search requests
+search_url = "http://api.iheart.com/api/v3/search/all"
+station_url = "https://api.iheart.com/api/v2/content/liveStations/"
+headers = {}
 
-# TODO: Change "Template" to a unique name for your skill
-class TemplateSkill(MycroftSkill):
+class IHeartRadioSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
-        super(TemplateSkill, self).__init__(name="TemplateSkill")
-        
-        # Initialize working variables used within the skill.
-        self.count = 0
+        super().__init__(name="IHeartRadioSkill")
+        self.vlc_instance = Instance()
+        self.vlc_player = self.vlc_instance.media_player_new()
+        self.station_name = None
+        self.station_id = None
+        self.stream_url = None
 
-    # The "handle_xxxx_intent" function is triggered by Mycroft when the
-    # skill's intent is matched.  The intent is defined by the IntentBuilder()
-    # pieces, and is triggered when the user's utterance matches the pattern
-    # defined by the keywords.  In this case, the match occurs when one word
-    # is found from each of the files:
-    #    vocab/en-us/Hello.voc
-    #    vocab/en-us/World.voc
-    # In this example that means it would match on utterances like:
-    #   'Hello world'
-    #   'Howdy you great big world'
-    #   'Greetings planet earth'
-    @intent_handler(IntentBuilder("").require("Hello").require("World"))
-    def handle_hello_world_intent(self, message):
-        # In this case, respond by simply speaking a canned response.
-        # Mycroft will randomly speak one of the lines from the file
-        #    dialogs/en-us/hello.world.dialog
-        self.speak_dialog("hello.world")
+    @intent_file_handler('StreamRequest.intent')
+    def handle_stream_intent(self, message):
+        self.find_station(message.data["station"])
+        LOG.debug("Station data: " + message.data["station"])
 
-    @intent_handler(IntentBuilder("").require("Count").require("Dir"))
-    def handle_count_intent(self, message):
-        if message.data["Dir"] == "up":
-            self.count += 1
-        else:  # assume "down"
-            self.count -= 1
-        self.speak_dialog("count.is.now", data={"count": self.count})
+    def find_station(self, search_term):
+        payload = { "keywords" : search_term, "maxRows" : 1, "bundle" : "false", "station" : "true", "artist" : "false", "track" : "false", "playlist" : "false", "podcast" : "false" }
+        # get the response from the IHeartRadio API
+        search_res = requests.get(search_url, params=payload, headers=headers)
+        search_obj = json.loads(search_res.text)
 
-    # The "stop" method defines what Mycroft does when told to stop during
-    # the skill's execution. In this case, since the skill's functionality
-    # is extremely simple, there is no need to override it.  If you DO
-    # need to implement stop, you should return True to indicate you handled
-    # it.
-    #
-    # def stop(self):
-    #    return False
+        self.station_name = search_obj["results"]["stations"][0]["name"]
+        self.station_id = search_obj["results"]["stations"][0]["id"]
+        LOG.debug("Station name: " + self.station_name + " ID: " + str(self.station_id))
+
+        # query the station URL using the ID
+        station_res = requests.get(station_url+str(self.station_id))
+        station_obj = json.loads(station_res.text)
+        # Use the first stream URL
+        for x in list(station_obj["hits"][0]["streams"])[0:1]:
+            self.stream_url = station_obj["hits"][0]["streams"][x]
+            break
+        LOG.debug("Station URL: " + self.stream_url)
+
+        # start playing the stream
+        Media = self.vlc_instance.media_new(self.stream_url)
+        Media.get_mrl()
+        self.vlc_player.set_media(Media)
+        self.vlc_player.play()
+
+    def stop(self):
+        self.vlc_player.stop();
+        return True
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
 def create_skill():
-    return TemplateSkill()
+    return IHeartRadioSkill()
